3d6b8f6ba4bf6323b895baa2c256e15b2e7da7b3
Teddy Otieno <tedotieno523@gmail.com>
2021-03-22 16:44:38 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ted 101 Demo App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Ted 101'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar
+        (title: Text(this.title), 
+        ),
+    body: Center(child: Text("Trial 101 First",)
+    ),
+    );
+  }
+}
+
